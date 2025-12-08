e2329f7c7f78b43ab0d575cafab414c38de94b27
Hernando Abella <hernandoabella@gmail.com>
2025-12-08 15:28:17 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import SideBar from "./components/SideBar";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div>
       <main>
-      <SideBar />
+        <Hero />
       </main>
     </div>
   );
